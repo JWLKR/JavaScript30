980e99e50926efd563294fc4b6ea19713d52d700
Johnathan Walker <walker.johnathanm@gmail.com>
2019-03-03 09:30:15 -0600

    added event listeners to add and remove class from elements. Working js drum machine in browser.

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,6 +1,17 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (!audio) return; // stop the function
-    audio.currentTime = 0;
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop the function from running alltogether
+    audio.currentTime = 0; // rewind to the start. without this only plays a sound every 10 keypresses or so
     audio.play();
-});
+    key.classList.add('playing');
+}
+
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
